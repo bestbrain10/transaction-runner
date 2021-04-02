2010980d788e55c870dd545c3b5526842a8bbda9
Livinus Igbaji <bestbrain10@gmail.com>
2021-04-02 22:45:08 +0100

    make index.js work as SDK

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 require('dotenv').config()
 
 const database = require('./database');
-const server = require('./server');
 
-database.authenticate()
-.then(() => {
-    server.listen(process.env.PORT, () => {
-        console.log(`Application Started on port ${process.env.PORT}`);
+if(require.main === module) {
+// if called from CLI activate server
+    const server = require('./server');
+    database.authenticate()
+    .then(() => {
+        server.listen(process.env.PORT, () => {
+            console.log(`Application Started on port ${process.env.PORT}`);
+        })
     })
-})
-.catch(e => {
-    console.log('Database connection failed');
-    console.log(e)
-})
+    .catch(e => {
+        console.log('Database connection failed');
+        console.log(e)
+    })
+} else {
+    // export SDK methods
 
+    module.exports = {
+        User: require('./models/user.model'),
+        Login: require('./models/login.model'),
+        UserTransaction: require('./models/transaction.model'),
+        databaseInit: () => database.authenticate(),
+        databaseClose: () => database.close()
+    }
+}
