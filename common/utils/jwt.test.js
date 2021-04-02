1fac39ad8d75999756af1c74c72fd57bf0e573b0
Livinus Igbaji <bestbrain10@gmail.com>
2021-04-02 17:19:11 +0100

    fix unit test for utils

---
+++ common/utils/jwt.test.js
@@ -0,0 +1,21 @@
+const key = 'coolkey';
+process.env.JWTKEY = key;
+const jwtLib = require('./jwt');
+const jwt = require('jsonwebtoken');
+
+
+describe('JWT', () => {
+    it('Can decode an input payload', () => {
+        const decodeSpy = jest.spyOn(jwt, 'verify').mockReturnValue('stuff');
+        const result = jwtLib.decode('hello');
+        expect(decodeSpy).toBeCalledWith('hello', key);
+        expect(result).toBe('stuff');
+    });
+
+    it('Can encode an input payload', () => {
+        const encodeSpy = jest.spyOn(jwt, 'sign').mockReturnValue('stuff');
+        const result = jwtLib.encode('hello');
+        expect(encodeSpy).toBeCalledWith('hello', key);
+        expect(result).toBe('stuff');
+    });
+});
