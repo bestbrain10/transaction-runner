8edb433777ce79ef436b813dd120d8e770b4af34
Livinus Igbaji <bestbrain10@gmail.com>
2021-04-02 21:49:30 +0100

    remove console.log from db configs

--- config/config.js
+++ config/config.js
@@ -8,19 +8,6 @@ const {
   DB_PORT,
   DB_SCHEMA
 } = process.env
-const fs = require('fs');
-
-console.log({
-  [NODE_ENV]: {
-    username: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_SCHEMA,
-    host: DB_HOST,
-    dialect: 'postgres',
-    port: DB_PORT
-  },
-  env: fs.readFileSync('.env', { encoding: 'utf8' })
-})
 
 module.exports = {
   [NODE_ENV]: {
